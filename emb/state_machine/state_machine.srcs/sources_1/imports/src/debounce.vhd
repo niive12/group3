8022ab9cf7b58e2a03b756cb3373194aebbeadf8
niive12 <niive12@student.sdu.dk>
2015-09-17 10:48:04 +0200

    double and long press works

--- emb/state_machine/state_machine.srcs/sources_1/imports/src/debounce.vhd
+++ emb/state_machine/state_machine.srcs/sources_1/imports/src/debounce.vhd
@@ -6,20 +6,26 @@ entity debounce is
             clk          : in  std_logic;
             ms_clk       : in  std_logic;
             btn          : in  std_logic;
-            btn_pushed   : out std_logic
+            btn_pushed   : out std_logic_vector(1 downto 0)
     );
 end debounce;     
 
 architecture Behavioral of debounce is
 
-type btn_state_t is (pressed, released);
+type btn_state_t is (pressed, released,semi_released,double_press);
 signal btn_state         : btn_state_t := released;
+
+
+constant btn_released    : std_logic_vector(1 downto 0) := "00";
+constant long_press      : std_logic_vector(1 downto 0) := "01";
+constant single_press    : std_logic_vector(1 downto 0) := "10";
+constant dbl_press       : std_logic_vector(1 downto 0) := "11";
 
 constant complete_empty  : std_logic_vector(3 downto 0) := (others => '0');
 constant complete_full   : std_logic_vector(3 downto 0) := (others => '1');
 signal continuity_vector : std_logic_vector(3 downto 0) := (others => '0');
 signal btn_status        : std_logic := '0';
-signal button_event      : std_logic := '0';
+signal button_event      : std_logic_vector(1 downto 0) := btn_released ;
 begin 
 
 btn_pushed <= button_event;
@@ -36,19 +42,59 @@ if rising_edge(ms_clk) then
 end if;
 end process;
 
-button_state_machine: process(clk)
--- variable sec_delay : integer range 1 to 1000 := 0;
+-- button_state_machine: process(ms_clk)
+-- begin
+-- if rising_edge(ms_clk) then
+--     if btn_state = pressed then
+--         button_event <= '0';
+--         if btn_status = '0' then
+--             btn_state <= released;
+--         end if;
+--     elsif btn_state = released then
+--         if btn_status = '1' then
+--             button_event <= '1';
+--             btn_state <= pressed;
+--         end if;
+--     end if;
+-- end if;
+-- end process;
+
+button_state_machine: process(ms_clk)
+variable set_delay : integer range 0 to 1000 := 0;
 begin
-if rising_edge(clk) then
-    if btn_state = pressed then
-        button_event <= '0';
-        if btn_status = '0' then
+if rising_edge(ms_clk) then
+    if btn_state = released then
+        button_event <= btn_released;
+        if btn_status = '1' then
+            btn_state <= pressed;
+            set_delay := 500;
+        end if;
+    elsif btn_state = pressed then
+        set_delay := set_delay - 1;
+        if set_delay = 0 then
+            button_event <= long_press;
             btn_state <= released;
         end if;
-    elsif btn_state = released then
-        if btn_status = '1' then
-            button_event <= '1';
-            btn_state <= pressed;
+        if btn_status = '0' then
+            button_event <= single_press;
+            btn_state <= semi_released;
+            set_delay := 500;
+        end if;
+    elsif btn_state = semi_released then
+        button_event <= btn_released;
+        set_delay := set_delay - 1;
+        if set_delay = 0 then
+            btn_state <= released;
+        end if;
+        if btn_status = '1' then 
+            button_event <= dbl_press;
+            btn_state <= double_press;
+        end if;
+    elsif btn_state = double_press then
+        button_event <= btn_released;
+        if btn_status = '0' then
+            btn_state <= released;
+            button_event <= btn_released;
         end if;
     end if;
 end if;
--- emb/state_machine/state_machine.srcs/sources_1/imports/src/main.vhd
+++ emb/state_machine/state_machine.srcs/sources_1/imports/src/main.vhd
@@ -26,6 +26,8 @@ entity main is
     Port (  
             clk          :  in  std_logic;
             btn          :  in  std_logic;
+            system_red   : out  std_logic;
+            system_yellow: out  std_logic;
             led          : out  std_logic_vector(5 downto 0)
     );
 end main;
@@ -35,8 +37,15 @@ architecture Behavioral of main is
 type main_state_t is (off, led1, led2, led3, led4, led5, led6); 
 signal main_state        : main_state_t := off;
 signal slow_clk          : std_logic;
-signal button_pushed     : std_logic;
+signal button_pushed     : std_logic_vector(1 downto 0);
 
+constant btn_released    : std_logic_vector(1 downto 0) := "00";
+constant long_press      : std_logic_vector(1 downto 0) := "01";
+constant single_press    : std_logic_vector(1 downto 0) := "10";
+constant double_press    : std_logic_vector(1 downto 0) := "11";
+
+signal reset_led         : std_logic := '0';
+signal warn_led          : std_logic := '0';
 component slow_clock is
     Port (  
             clk          :  in  std_logic;
@@ -49,7 +58,7 @@ component debounce is
             clk          : in  std_logic;
             ms_clk       : in  std_logic;
             btn          : in  std_logic;
-            btn_pushed   : out std_logic
+            btn_pushed   : out std_logic_vector(1 downto 0)
     );
 end component;     
 
@@ -68,42 +77,51 @@ btn_controller: debounce
             btn          => btn            ,
             btn_pushed   => button_pushed
     );
+
+system_red <= reset_led;
+system_yellow <= warn_led;
     
-main_state_machine: process(clk)
+main_state_machine: process(slow_clk)
 begin
-if rising_edge(clk) then
-    if main_state = off then
-        if button_pushed = '1' then
+if rising_edge(slow_clk) then
+    if button_pushed = long_press then
+        reset_led <= not reset_led;
+        main_state <= off;
+    elsif button_pushed = double_press then
+        warn_led <= not warn_led;
+        main_state <= off;
+    elsif main_state = off then
+        led <= (others => '0');
+        if button_pushed = single_press then
             main_state <= led1;
-            led <= (others => '0');
         end if;
     elsif main_state = led1 then
-        if button_pushed = '1' then
+        if button_pushed = single_press then
             main_state <= led2;
             led <= "000001";
         end if;
     elsif main_state = led2 then
-        if button_pushed = '1' then
+        if button_pushed = single_press then
             main_state <= led3;
             led <= "000010";
         end if;
     elsif main_state = led3 then
-        if button_pushed = '1' then
+        if button_pushed = single_press then
             main_state <= led4;
             led <= "000100";
         end if;
     elsif main_state = led4 then
-        if button_pushed = '1' then
+        if button_pushed = single_press then
             main_state <= led5;
             led <= "001000";
         end if;
     elsif main_state = led5 then
-        if button_pushed = '1' then
+        if button_pushed = single_press then
             main_state <= led6;
             led <= "010000";
         end if;
     elsif main_state = led6 then
-        if button_pushed = '1' then
+        if button_pushed = single_press then
             main_state <= led1;
             led <= "100000";
         end if;
