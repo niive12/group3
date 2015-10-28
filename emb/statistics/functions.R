f335a4b4f049b639612642c3f72fb701448e176e
niive12 <niive12@student.sdu.dk>
2015-10-28 22:03:01 +0100

    made a graph... it is not as pretty as I have hoped...

---
+++ emb/statistics/functions.R
@@ -0,0 +1,108 @@
+find_event <- function(data, width=5, direction="both",index=FALSE){
+    min = min(data[,5])
+    max = max(data[,5])
+    tolerance = 1.5
+    half = trunc(width/2)
+    f_events = 0
+    r_events = 0
+    last_mean = mean(data[0:width,5])
+    last_f = TRUE
+    last_r = TRUE
+    for(i in seq(width,(dim(data)[1]-half-1),by=width)){
+        mean = mean(data[(i-half):(i+half),5]) 
+        if( ((-tolerance) < (mean-last_mean)) #falling
+          ){
+            if(last_f == FALSE){
+                if(index ==TRUE){
+                    f_events = c(f_events,i)
+                } else {
+                    f_events = c(f_events,data[i,4])
+                }
+            }
+            last_f = TRUE
+        } else {
+            last_f = FALSE
+        }
+        if( ((mean-last_mean) < (tolerance)) #rising
+          ){
+            if(last_r == FALSE){
+                if(index ==TRUE){
+                    r_events = c(r_events,i)
+                } else {
+                    r_events = c(r_events,data[i,4])
+                }
+            }
+            last_r = TRUE
+        } else {
+            last_r = FALSE
+        }
+        last_mean = mean
+    }
+    f_events = f_events[-1]
+    r_events = r_events[-1]
+    if(direction == "falling"){
+        events = f_events
+    } else if(direction == "rising"){
+        events = r_events
+    } else {
+        events = c(r_events,f_events)
+    }
+    return(events)
+}
+
+time_between_edges <- function(edges){
+    time = edges[-1] #mainly for length purposes
+    for(i in 2:length(edges) ){
+        time[i-1] = edges[i] - edges[i-1]
+    }
+    time = time * 10^9 # nano seconds
+    return(time)
+}
+
+rms <- function(data){
+    n = length(data[,5])
+    v = sqrt(1/n * sum( data[,5] * data[,5] ) )
+    return(v)
+}
+
+bit_value <- function(data_signal, clock_signal, start_edge, clock_skips=0, clocks){
+    clk_rising_edge = find_event(clock_signal,direction="rising",index=TRUE)
+    if(length(clk_rising_edge) < (clocks+clock_skips)){ stop("Error: The signal has not been meassured for that long") }
+    logic_low  = 0.7 
+    logic_high = 2.5
+    start = which(data_signal[,4]>start_edge)[1]
+    pulse_width = clk_rising_edge[2] - clk_rising_edge[1]
+    read_time = trunc(pulse_width)/4
+    bit_val = c("0b")
+    for(i in clock_skips:(clocks-1)){
+        index = start+i*pulse_width
+        if(data_signal[index,5] > logic_high){
+            bit_val = c(bit_val,"1")
+        } else if(data_signal[index,5] < logic_low){
+            bit_val = c(bit_val,"0")
+        } else {
+            bit_val = c(bit_val,"U")
+        }
+    }
+    return(paste(bit_val,collapse=""))
+}
+
+binary_to_int <- function(bit_string){
+    int = 0;
+    split = strsplit(bit_string, "")[[1]]
+    garbage = FALSE
+    for (bit in length(split):3) { #ignoring the 0b
+        int = int * 2 #leftshift
+        if(split[bit] == "1"){
+            int = int + 1 #add one
+        } else if(split[bit] == "0"){
+            # do nothing
+        } else {
+            garbage = TRUE
+        }
+    }
+    if(garbage){
+        int = -1
+    }
+    return(int)
+}
--- emb/statistics/stat.R
+++ emb/statistics/stat.R
@@ -1,114 +1,7 @@
 library(stringr)
+source('functions.R')
 
-find_event <- function(data, width=5, direction="both",index=FALSE){
-    min = min(data[,5])
-    max = max(data[,5])
-    tolerance = 1.5
-    half = trunc(width/2)
-    f_events = 0
-    r_events = 0
-    last_mean = mean(data[0:width,5])
-    last_f = TRUE
-    last_r = TRUE
-    for(i in seq(width,(dim(data)[1]-half-1),by=width)){
-        mean = mean(data[(i-half):(i+half),5]) 
-        if( ((-tolerance) < (mean-last_mean)) #falling
-          ){
-            if(last_f == FALSE){
-                if(index ==TRUE){
-                    f_events = c(f_events,i)
-                } else {
-                    f_events = c(f_events,data[i,4])
-                }
-            }
-            last_f = TRUE
-        } else {
-            last_f = FALSE
-        }
-        if( ((mean-last_mean) < (tolerance)) #rising
-          ){
-            if(last_r == FALSE){
-                if(index ==TRUE){
-                    r_events = c(r_events,i)
-                } else {
-                    r_events = c(r_events,data[i,4])
-                }
-            }
-            last_r = TRUE
-        } else {
-            last_r = FALSE
-        }
-        last_mean = mean
-    }
-    f_events = f_events[-1]
-    r_events = r_events[-1]
-    if(direction == "falling"){
-        events = f_events
-    } else if(direction == "rising"){
-        events = r_events
-    } else {
-        events = c(r_events,f_events)
-    }
-    return(events)
-}
-
-time_between_edges <- function(edges){
-    time = edges[-1] #mainly for length purposes
-    for(i in 2:length(edges) ){
-        time[i-1] = edges[i] - edges[i-1]
-    }
-    time = time * 10^9 # nano seconds
-    return(time)
-}
-
-rms <- function(data){
-    n = length(data[,5])
-    v = sqrt(1/n * sum( data[,5] * data[,5] ) )
-    return(v)
-}
-
-bit_value <- function(data_signal, clock_signal, start_edge, clock_skips=0, clocks){
-    clk_rising_edge = find_event(clock_signal,direction="rising",index=TRUE)
-    if(length(clk_rising_edge) < (clocks+clock_skips)){ stop("Error: The signal has not been meassured for that long") }
-    logic_low  = 0.7 
-    logic_high = 2.5
-    start = which(data_signal[,4]>start_edge)[1]
-    pulse_width = clk_rising_edge[2] - clk_rising_edge[1]
-    read_time = trunc(pulse_width)/4
-    bit_val = c("0b")
-    for(i in clock_skips:(clocks-1)){
-        index = start+i*pulse_width
-        if(data_signal[index,5] > logic_high){
-            bit_val = c(bit_val,"1")
-        } else if(data_signal[index,5] < logic_low){
-            bit_val = c(bit_val,"0")
-        } else {
-            bit_val = c(bit_val,"U")
-        }
-    }
-    return(paste(bit_val,collapse=""))
-}
-
-binary_to_int <- function(bit_string){
-    int = 0;
-    split = strsplit(bit_string, "")[[1]]
-    garbage = FALSE
-    for (bit in length(split):3) { #ignoring the 0b
-        int = int * 2 #leftshift
-        if(split[bit] == "1"){
-            int = int + 1 #add one
-        } else if(split[bit] == "0"){
-            # do nothing
-        } else {
-            garbage = TRUE
-        }
-    }
-    if(garbage){
-        int = -1
-    }
-    return(int)
-}
-
+cat("---------- Proof of concept ----------\n")
 scope_clk = 3; scope_vin = 1; scope_cs = 2; scope_miso = 4; 
 i = 21
 
@@ -142,3 +35,27 @@ bit = bit_value(data_signal = miso, clock_signal= clk, start_edge = cs_events[1]
 print(bit)
 print(binary_to_int(bit))
 plot(miso[,4],miso[,5],type="l")
+
+number_of_meassurements = 34
+bit_value_vs_vin = matrix(0,number_of_meassurements,2)
+for(i in 1:number_of_meassurements){
+        filename = paste(c("data/C",scope_clk,"adc_testttt",str_pad(i, 5, pad="0"),".csv"),collapse="")
+        clk = read.csv(filename,skip=6)
+        filename = paste(c("data/C",scope_cs,"adc_testttt",str_pad(i, 5, pad="0"),".csv"),collapse="")
+        cs = read.csv(filename,skip=6)
+        filename = paste(c("data/C",scope_miso,"adc_testttt",str_pad(i, 5, pad="0"),".csv"),collapse="")
+        miso = read.csv(filename,skip=6)
+        filename = paste(c("data/C",scope_vin,"adc_testttt",str_pad(i, 5, pad="0"),".csv"),collapse="")
+        vin = read.csv(filename,skip=6)
+        
+        cs_events = find_event(cs,direction="falling")
+        bit = bit_value(data_signal = miso, clock_signal= clk, start_edge = cs_events[1], clock_skips=7, clocks=17)
+        bit_value_vs_vin[i,1] = binary_to_int(bit)
+        bit_value_vs_vin[i,2] = rms(vin)
+        print(bit_value_vs_vin[i,])
+}
+
+adc_value  = bit_value_vs_vin[,1]
+v_in       = bit_value_vs_vin[,2]
+
+plot(v_in,adc_value,type="l")
