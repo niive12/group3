cd2bd749094c0d65d5d80b74b64e291dbb671451
mahes12 <mahes12@student.sdu.dk>
2015-11-05 09:04:47 +0100

    Made more structure in the folder

--- emb/src/button_type.vhd
+++
@@ -1,5 +0,0 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-package button_type is
-    type btn_type is (btn_released,long_press,single_press,double_press);
-end package;
--- emb/state_machine/state_machine.srcs/sources_1/imports/src/button_type.vhd
+++
@@ -1,4 +0,0 @@
-library IEEE;
-package button_type is
-    type btn_type is (btn_released,long_press,single_press,double_press);
-end package;
