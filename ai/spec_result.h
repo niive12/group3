e95e8fad4c05f3620a1f233056a3833d8114a657
niive12 <niive12@student.sdu.dk>
2015-11-16 22:53:36 +0100

    fixed error manually in file, will fix in code later. The spec_result.h can be used for testing

--- ai/spec_result.h
+++ ai/spec_result.h
@@ -1,1 +1,3 @@
-const char[v] = {l , u , u , R , R , R , d , r , r , u , u , u , l , l , l , L , D , u , r , r , r , r , d , d , d , l , l , u , l , L , u , l , l , u , R , R , R , R , R , u , r , D , l , l , l , l , d , d , D , l , U , r , r , r , d , r , r , u , L , L , L , L , u , u , r , r , r , r , D , u , l , l , l , l , d , d , d , l , d , d , r , U , U , U , r , r , r , R , u , u , l , l , l , l , D , u , l , l , d , R , D , R , R , R , l , l , l , u , u , r , D , l , d , R , d , d , l , U}
+const char[118] = {l , u , u , R , R , R , d , r , r , u , u , u , l , l , l , L , D , u , r , r , r , r , d , d , d , l , l , u , l , L , u , l , l , u , R , R , R , R , R , u , r , D , 
+l 
+, l , l , l , d , d , D , l , U , r , r , r , d , r , r , u , L , L , L , L , u , u , r , r , r , r , D , u , l , l , l , l , d , d , d , l , d , d , r , U , U , U , r , r , r , R , u , u , l , l , l , l , D , u , l , l , d , R , D , R , R , R , l , l , l , u , u , r , D , l , d , R , d , d , l , U}
