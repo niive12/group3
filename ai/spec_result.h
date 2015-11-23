33249b3a29fe6e73fabc90be19e5506a1ae83bf2
niive12 <niive12@student.sdu.dk>
2015-11-23 12:17:00 +0100

    updated solution so it is for the 2015 map

--- ai/spec_result.h
+++ ai/spec_result.h
@@ -1,3 +1,4 @@
-const char[118] = {l , u , u , R , R , R , d , r , r , u , u , u , l , l , l , L , D , u , r , r , r , r , d , d , d , l , l , u , l , L , u , l , l , u , R , R , R , R , R , u , r , D , 
-l 
-, l , l , l , d , d , D , l , U , r , r , r , d , r , r , u , L , L , L , L , u , u , r , r , r , r , D , u , l , l , l , l , d , d , d , l , d , d , r , U , U , U , r , r , r , R , u , u , l , l , l , l , D , u , l , l , d , R , D , R , R , R , l , l , l , u , u , r , D , l , d , R , d , d , l , U}
+const char path[081] = {d , r , d , d , l , l , l , L , U , d , d , l , l , u , R , R , 
+R , R , R , d , r , U , U , r , u , u , l , l , d , R , d , d , l , l , u , L , u , u , 
+l , l , d , D , R , d , d , l , l , u , R , R , R , R , R , d , r , U , U , U , l , u , 
+R , d , d , d , l , l , u , L , u , L , D , l , d , R , R , R , R , d , r , U , U}
